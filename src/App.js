3491ad6015679d80c218cebe7b05e113e4fdbb75
ines2024 <ines.lahbib7@gmail.com>
2023-11-22 21:04:21 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'; 
+import AddTask from './Components/AddTask';
+import Filter from './Components/Filter';
+import ListTask from './Components/ListTask';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <div style={{paddingTop:'20px'}}>
+          <AddTask/>
+          <Filter/>
+        </div>
+        <ListTask/>
+      </div>
     </div>
   );
 }
